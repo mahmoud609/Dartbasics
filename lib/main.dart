847d91d2662c6c4b8151bbfcd03a12b2dda7d42c
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-11-26 20:23:38 +0200

    basics dart part 1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  // create print Statement
+
+  print('hhhhhhhhhhhhh');
+
+// syntax
+  // print('this print statement in dart');
+  // print('this print statement in python')
+  // <?php
+  // echo "this print statement in php";
+  // ?>
+}
